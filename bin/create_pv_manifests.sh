2bd32b4d03522f16a8cf910c58c99e25b55e020e
Ezekiel Newren <ezekielnewren@gmail.com>
2024-05-05 15:42:26 -0600

    create persisten volumes and persisten volume claims for rook-ceph for each device defined in vault

---
+++ bin/create_pv_manifests.sh
@@ -0,0 +1,44 @@
+NAMESPACE="$1"
+
+if [ "$NAMESPACE" == "" ]; then
+    echo "you must supply a namespace"
+    exit 1
+fi
+
+host="pm1 pm2 pm3"
+
+data=$(echo "$host" | tr ' ' '\n' | xargs -I {} vault kv get kv/machine/{} | jq .data.data | jq -sMc .)
+
+template=pv-template.yaml
+
+
+create_manifest() {
+  NODE=$1
+  CLASS=$2
+  MNSN=$3
+  DESIGNATION=$(echo $MNSN | tr '[[:upper:]]' '[[:lower:]]' | tr '_' '-')
+  SIZE=$(ssh metal@$NODE sudo blockdev --getsize64 /dev/mapper/$MNSN)
+  filename=pv-$NODE-$CLASS.$DESIGNATION.yaml
+  echo $filename
+  < $template sed -e "s/NODE/$NODE/g" -e "s/CLASS/$CLASS/g" -e "s/MNSN/$MNSN/g" -e "s/DESIGNATION/$DESIGNATION/g" -e "s/SIZE/$SIZE/g" -e "s/NAMESPACE/$NAMESPACE/g" > $filename
+}
+
+
+for name in $host; do
+    class="hdd"
+    for mnsn in $(echo "$data" | jq ".[] | select(.hostname == \"$name\")" | jq -r .fde.hdd[].mnsn); do
+        create_manifest $name $class $mnsn
+    done
+
+    class="ssd"
+    for mnsn in $(echo "$data" | jq ".[] | select(.hostname == \"$name\")" | jq -r .fde.ssd[].mnsn); do
+        create_manifest $name $class $mnsn
+    done
+
+    class="nvme"
+    for mnsn in $(echo "$data" | jq ".[] | select(.hostname == \"$name\")" | jq -r .fde.nvme[].mnsn); do
+        create_manifest $name $class $mnsn
+    done
+done
+
+
