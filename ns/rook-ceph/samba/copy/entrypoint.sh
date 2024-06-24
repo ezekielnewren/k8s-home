b955fa5212a298cfe7bfa6ffd1c32b09a128373e
Ezekiel Newren <ezekielnewren@gmail.com>
2024-06-24 14:18:31 -0600

    create container that bridges ceph with samba

--- ns/rook-ceph/samba/copy/entrypoint.sh
+++ ns/rook-ceph/samba/copy/entrypoint.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+if [ "$SAMBA_UID" == "" ]; then
+    SAMBA_UID=1000
+fi
+
+if [ "$SAMBA_GID" == "" ]; then
+    SAMBA_GID=1000
+fi
+
 if [ $(id -u) -eq 0 ]; then
-    USERID=1000
-    useradd -u $USERID -s /bin/bash samba
+    groupadd -g $SAMBA_GID samba
+    useradd -u $SAMBA_UID -g $SAMBA_GID -s /bin/bash samba
     echo -e "$SAMBA_PASSWORD\n$SAMBA_PASSWORD" | smbpasswd -a samba
+    echo -e "[client.samba]\n    key = $SAMBA_PASSWORD" > /etc/ceph/keyring
 
-    mkdir -p /var/log/samba
-    chown -R $USERID:$USERID /var/log/samba
-
-    mkdir -p /var/lib/samba
-    chown -R $USERID:$USERID /var/lib/samba
-
-    mkdir -p /run/samba
-    chown -R $USERID:$USERID /run  ## the entire run directory needs to be read-writable for /run/smbd.pid
-    chown -R $USERID:$USERID /mnt/media /mnt/transfer
-    # exec dumb-init bash
-    exec su - samba -c /entrypoint.sh
+    mkdir -p /var/log/samba /var/lib/samba /run/samba
+    chown -R samba:samba /var/log/samba /var/lib/samba /run /mnt/media /mnt/transfer /etc/ceph/keyring
+    chmod 600 /etc/ceph/keyring
+    
+    # bash
 else
-    common="--name client.samba -m pm1:6789,pm2:6789,pm3:6789 --keyring /etc/ceph/keyring"
+    # common="-f --name client.samba -m pm1:6789,pm2:6789,pm3:6789 --keyring /etc/ceph/keyring"
+    common="--name client.samba"
     ceph-fuse $common --client_fs=hddfs --client-mountpoint=/media /mnt/media
     ceph-fuse $common --client_fs=ssdfs --client-mountpoint=/transfer /mnt/transfer
-
-    # exec dumb-init bash
-    smbd --interactive --debug-stdout --debuglevel 3
-    # exec dumb-init su - samba -c bash -c "smbd --foreground --debug-stdout --debuglevel 3"
-    # exec dumb-init su - samba -c bash -c "smbd --interactive --debug-stdout --debuglevel 3"
+    :
+    smbd --foreground --debug-stdout --debuglevel 3 --no-process-group
 fi
+
--- ns/rook-ceph/samba/run.sh
+++ ns/rook-ceph/samba/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # --cap-add SYS_ADMIN
-docker run --name samba --device /dev/fuse --cap-add SYS_ADMIN -e SAMBA_PASSWORD=$SAMBA_PASSWORD --rm -it -v /tmp/$USER/keyring:/etc/ceph/keyring ceph_to_samba
+docker run --name samba --device /dev/fuse --cap-add SYS_ADMIN -e SAMBA_UID -e SAMBA_GID -e SAMBA_PASSWORD --rm -it ceph_to_samba
 
 
