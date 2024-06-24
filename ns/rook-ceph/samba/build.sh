6c3a69e142c63c6635404fa2f13ca015d892a46d
Ezekiel Newren <ezekielnewren@gmail.com>
2024-06-24 16:26:44 -0600

    add ceph_to_samba service

--- ns/rook-ceph/samba/build.sh
+++ ns/rook-ceph/samba/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker build --network host -t ceph_to_samba .
-
+docker tag ceph_to_samba docker.ezekielnewren.com/ceph_to_samba:latest
+echo docker push docker.ezekielnewren.com/ceph_to_samba:latest
 
